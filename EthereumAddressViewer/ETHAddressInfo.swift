1bcef1569f85c9b2f73e84a7ba2fc5e574fa84e2
Christopher Vensand <chrisvensand@gmail.com>
2018-07-08 23:30:08 -0700

    removed ETHAddressInfo.swift and EthplorerDescription.swift

--- EthereumAddressViewer/ETHAddressInfo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ETHAddressInfo.swift
-//  EthereumAddressViewer
-//
-//  Created by Christopher Vensand on 7/7/18.
-//  Copyright © 2018 Christopher Vensand. All rights reserved.
-//
-
-import Foundation
--- EthereumAddressViewer/EthplorerDescription.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EthplorerDescription.swift
-//  EthereumAddressViewer
-//
-//  Created by Christopher Vensand on 7/7/18.
-//  Copyright © 2018 Christopher Vensand. All rights reserved.
-//
-
-import Foundation
