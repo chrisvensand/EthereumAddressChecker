2fd53e285716da27e1bfb190c3530c119f334799
Christopher Vensand <chrisvensand@gmail.com>
2018-07-09 18:16:02 -0700

    Removed ETHAddressInfo from class

--- EthereumAddressViewer/AddressTracker.swift
+++ EthereumAddressViewer/AddressTracker.swift
@@ -12,20 +12,43 @@ import Foundation
 class AddressTracker {
     
     // Address variables displayed on button in homepage
-    var name: String
-    var address: String
-    var totalBalance: float_t
-    
-    var ethAddress: ETHAddress
+    var name = ""
+    var address = ""
+    var totalValue = Float(0)
+    var ethAddress = ETHAddressInfo()
     
     init (name: String, publicAddress: String) {
         self.name = name
         self.address = publicAddress
         
-        // Retrieve Ethereum Address data
-        ethAddress = ETHAddress(publicAddress: publicAddress)
+        let jsonUrlString = "https://api.ethplorer.io/getAddressInfo/" + address + "?apiKey=freekey"
+        let urlString = URL(string: jsonUrlString)
         
-        self.totalBalance = ethAddress.totalBalance()
+        URLSession.shared.dataTask(with: urlString!) { (data, response, Error) in
+            
+            do {
+                self.ethAddress = try JSONDecoder().decode(ETHAddressInfo.self, from: data!)
+            }
+                // If URLSession data cannot be retrieved print error
+            catch let jsonErr {
+                print(jsonErr)
+            }
+        }.resume()
+        
+        
+        //self.totalBalance = findBalance()
+        
+    }
+    
+    func findBalance() -> float_t {
+        
+        var totalBalance = ethAddress.ETH!.balance!
+        
+        for token in ethAddress.tokens! {
+            totalBalance += token.balance!
+        }
+        
+        return totalBalance
     }
     
 }
